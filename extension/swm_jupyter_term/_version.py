c3e277d20519aead18e19ad152258591aa282644
Taras Shapovalov <taras@iclouds.net>
2021-12-26 13:47:50 +0300

    Add checking and formatting for the extension in python

--- extension/swm_jupyter_term/_version.py
+++ extension/swm_jupyter_term/_version.py
@@ -1,23 +1,22 @@
 import json
+import typing
 from pathlib import Path
 
 __all__ = ["__version__"]
 
-def _fetchVersion():
+
+def _fetchVersion() -> typing.Any:
     HERE = Path(__file__).parent.resolve()
 
-    for settings in HERE.rglob("package.json"): 
+    for settings in HERE.rglob("package.json"):
         try:
             with settings.open() as f:
                 version = json.load(f)["version"]
-                return (
-                    version.replace("-alpha.", "a")
-                    .replace("-beta.", "b")
-                    .replace("-rc.", "rc")
-                )
+                return version.replace("-alpha.", "a").replace("-beta.", "b").replace("-rc.", "rc")
         except FileNotFoundError:
             pass
 
     raise FileNotFoundError(f"Could not find package.json under dir {HERE!s}")
+
 
 __version__ = _fetchVersion()
--- extension/swm_jupyter_term/handlers.py
+++ extension/swm_jupyter_term/handlers.py
@@ -1,19 +1,17 @@
 import json
 import platform
-import httpx
 
+import httpx
+import tornado
 from jupyter_server.base.handlers import APIHandler
 from jupyter_server.utils import url_path_join
+from notebook.notebookapp import NotebookApp
 from swmclient.api import SwmApi
-from swmclient.generated.types import File
-import tornado
 
-class RouteHandler(APIHandler):
-    # The following decorator should be present on all verb methods (head, get, post,
-    # patch, put, delete, options) to ensure only authorized user can request the
-    # Jupyter server
-    @tornado.web.authenticated
-    def get(self):
+
+class RouteHandler(APIHandler):  # type: ignore
+    @tornado.web.authenticated  # type: ignore
+    def get(self) -> None:
         url = f"https://{platform.node()}:8443"
         swm_api = SwmApi(
             url=url,
@@ -28,13 +26,13 @@ class RouteHandler(APIHandler):
                 for job in jobs:
                     output.append(job.to_dict())
             else:
-                output = "No jobs found"
+                output = ["No jobs found"]
         except httpx.ConnectError as e:
-            output = f"Can't connect to swm-core ({url}): {e}"
+            output = [f"Can't connect to swm-core ({url}): {e}"]
         self.finish(json.dumps({"data": output}))
 
 
-def setup_handlers(web_app):
+def setup_handlers(web_app: NotebookApp) -> None:
     host_pattern = ".*$"
 
     base_url = web_app.settings["base_url"]
