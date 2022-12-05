235d38f7eb7d9856341e2481fdec3639efbcd573
Taras Shapovalov <taras@iclouds.net>
2022-12-05 14:30:26 +0000

    Use DummyAuthenticator for easier development purposes

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -10,7 +10,9 @@ c.JupyterHub.hub_ip = '0.0.0.0'
 c.JupyterHub.hub_port = 8081
 c.JupyterHub.log_level = 'DEBUG'
 c.JupyterHub.spawner_class = 'swm_jupyter_spawner.spawner.SwmSpawner'
+c.JupyterHub.authenticator_class = 'jupyterhub.auth.DummyAuthenticator'
 
 c.SwmSpawner._swm_ca_file = '~/.swm/spool/secure/cluster/ca-chain-cert.pem'
 c.SwmSpawner._swm_key_file = '~/.swm/key.pem'
 c.SwmSpawner._swm_cert_file = '~/.swm/cert.pem'
+
